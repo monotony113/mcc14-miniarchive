0636429dc38f6aeb62c43500e4803e4f3821df87
Tony Wu <tony.wu@nyu.edu>
2018-10-21 17:16:47 -0400

    CORS

--- main.js
+++ main.js
@@ -1,7 +1,44 @@
-$(document).ready(function(){
-    $("button").click(function(){
-        $.get("demo_test.asp", function(data, status){
-            alert("Data: " + data + "\nStatus: " + status);
-        });
-    });
-});
+/* global $ window document */
+
+function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+
+        // Check if the XMLHttpRequest object has a "withCredentials" property.
+        // "withCredentials" only exists on XMLHTTPRequest2 objects.
+        xhr.open(method, url, true);
+
+    } else if (typeof XDomainRequest != "undefined") {
+
+        // Otherwise, check if XDomainRequest.
+        // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+
+    } else {
+
+        // Otherwise, CORS is not supported by the browser.
+        xhr = null;
+
+    }
+    return xhr;
+}
+
+var xhr = createCORSRequest('GET', 'https://wp.nyu.edu/mcc014f18_tw_miniarchive/wp-json/wp/v2/posts?category=photos');
+if (!xhr) {
+    throw new Error('CORS not supported');
+}
+
+xhr.withCredentials = true;
+
+xhr.onload = function () {
+    var responseText = xhr.responseText;
+    alert(responseText);
+    // process the response.
+};
+
+xhr.onerror = function () {
+    alert('There was an error!');
+};
+
+xhr.send();
