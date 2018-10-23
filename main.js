e4a9a75825cbb8b37976d591aa755a91d573ad43
monotony-23113 <tonywuyt126@gmail.com>
2018-10-23 05:47:54 -0400

    Lots of changes

--- main.js
+++ main.js
@@ -5,11 +5,38 @@ $(document).ready(() => {
 	"use strict";
 
 	var imageList = [];
+	var collections = {};
+	var tags = {};
 
-	function ImagePost(id, pageUrl, imgPostUrl) {
+	function ImagePost(id, title, content, categories, tags, pageUrl, imgPostUrl) {
 		this.id = id;
+		this.title = title;
+        var contentText = $(content).prop("innerText");;
+        this.metadata = {
+            date: (() => { try { return new Date(contentText.match(/Recorded:.(.*)/)[1]) } catch(err) { return null } } )(),
+            location: (() => { try { return contentText.match(/Location:.(.*)/)[1] } catch(err) { return null } } )(),
+            desc: (() => { try { return contentText.match(/Description:.(.*)/)[1] } catch(err) { return null } } )()
+        }
+		this.metadata.collections = categories;
+		this.metadata.tags = tags;
 		this.pageUrl = pageUrl;
 		this.imgPostUrl = imgPostUrl;
+	};
+
+	function refresh(url) {
+		fetchPosts(url)
+			.then((data) => {
+				makeImageListFrom(data);
+			})
+			.then(() => {
+				$("#photo-list").empty();
+				imageList.forEach((item, index) => {
+					$("#photo-list").append(createImageTile(item));
+				})
+				imageList.forEach((item, index) => {
+					fillImageTile(item);
+				})
+			});
 	};
 
 	function fetchPosts(url) {
@@ -31,15 +58,19 @@ $(document).ready(() => {
 		});
 	};
 
-    function makeImageListFrom(data) {
-        return new Promise((resolve, reject) => {
-            console.log(data);
-            data.forEach((item, index) => {
-                var imagePost = new ImagePost(`photo-${index}`, item.link, item._links["wp:featuredmedia"][0].href);
-                imageList.push(imagePost);
-            });
-        });
-    }
+	function makeImageListFrom(data) {
+		return new Promise((resolve, reject) => {
+			console.log(data);
+			data.forEach((item, index) => {
+				try {
+					var imagePost = new ImagePost(`photo-${index}`, item.title.rendered, item.content.rendered, item.categories, item.tags, item.link, item._links["wp:featuredmedia"][0].href);
+					imageList.push(imagePost);
+				} catch (err) {
+					console.error(err);
+				}
+			});
+		});
+	}
 
 	function getThumbnailUrl(imagePost) {
 		return new Promise((resolve, reject) => {
@@ -63,29 +94,42 @@ $(document).ready(() => {
 	function createImageTile(imagePost) {
 		var tileContainer = $("<div>").addClass("tile-container").attr('id', imagePost.id);
 		var image = $("<img>").addClass("image-content");
-		var info = $("<span>").addClass("info-content").attr("display", "none");
-		var tile = $(tileContainer).append(image).append(info);
+		var info = $("<span>").addClass("info-content");
+		var title = $("<span>").addClass("image-title");
+		var tile = $(tileContainer).append(title).append(info).append(image);
 		return tile;
 	};
 
-    function fillImageTile(imagePost) {
-        getThumbnailUrl(imagePost).then((thumbnailUrl) => {
-            imagePost.thumbnailUrl = thumbnailUrl;
-            $(`#${imagePost.id} img`).attr("src", thumbnailUrl);
-        })
-    }
+	function fillImageTile(imagePost) {
+        $(`#${imagePost.id} .image-title`).text(imagePost.title);
+        var dateString = imagePost.metadata.date ? imagePost.metadata.date.toDateString() : "Unknown date";
+        var locationString = imagePost.metadata.location || "Unknown location";
+        var descString = imagePost.metadata.desc || "No description";
+        $(`#${imagePost.id} .info-content`).append(`<p>${dateString}<br>${locationString}<br>${descString}</p>`);
+		getThumbnailUrl(imagePost).then((thumbnailUrl) => {
+			imagePost.thumbnailUrl = thumbnailUrl;
+			$(`#${imagePost.id} .image-content`).attr("src", thumbnailUrl);
+		})
+	}
 
-	fetchPosts("https://wp.nyu.edu/mcc014f18_tw_miniarchive/wp-json/wp/v2/posts?categories=5&per_page=20")
+	fetchPosts("https://wp.nyu.edu/mcc014f18_tw_miniarchive/wp-json/wp/v2/categories")
 		.then((data) => {
-            makeImageListFrom(data);
-        })
-        .then(() => {
-            imageList.forEach((item, index) => {
-                $("#photo-list").append(createImageTile(item));
-            })
-            imageList.forEach((item, index) => {
-                fillImageTile(item);
-            })
-        });
+			data.forEach((item, index) => {
+				if (item.parent === 5) {
+					collections[`${item.id}`] = item.name;
+				};
+			});
+			console.log(collections);
+		});
+	fetchPosts("https://wp.nyu.edu/mcc014f18_tw_miniarchive/wp-json/wp/v2/tags")
+		.then((data) => {
+			data.forEach((item, index) => {
+				tags[`${item.id}`] = item.name;
+			});
+			console.log(tags);
+		});
+
+
+	refresh("https://wp.nyu.edu/mcc014f18_tw_miniarchive/wp-json/wp/v2/posts?categories=5&per_page=20");
 
 });
