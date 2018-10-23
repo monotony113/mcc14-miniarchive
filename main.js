2bcaeba09fee86d3e3de199fcbf3a6ade1637c89
Tony Wu <tony.wu@nyu.edu>
2018-10-22 20:59:12 -0400

    Post-github-outage commit

--- main.js
+++ main.js
@@ -1,58 +1,34 @@
 /* global $ window document xhr alert XMLHttpRequest XDomainRequest */
 
-function createCORSRequest(method, url) {
-    var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr) {
-
-        // Check if the XMLHttpRequest object has a "withCredentials" property.
-        // "withCredentials" only exists on XMLHTTPRequest2 objects.
-        xhr.open(method, url, true);
-
-    } else if (typeof XDomainRequest != "undefined") {
-
-        // Otherwise, check if XDomainRequest.
-        // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-
-    } else {
-
-        // Otherwise, CORS is not supported by the browser.
-        xhr = null;
-
-    }
-    return xhr;
-}
-
 var list = undefined
 
 $(document).ready(() => {
 
-    var xhr = createCORSRequest('GET', 'https://wp.nyu.edu/mcc014f18_tw_miniarchive/wp-json/wp/v2/posts?categories=5');
-    if (!xhr) {
-        throw new Error('CORS not supported');
-    }
+    $.ajax({
+        method: 'GET',
+        url: 'https://wp.nyu.edu/mcc014f18_tw_miniarchive/wp-json/wp/v2/posts?categories=5&per_page=20',
+        contentType: 'text/plain',
+        xhrFields: {
+            withCredentials: true
+        },
+        success: (data, status) => {
+            console.log(data)
+            data.forEach((item, index) => {
+                var newLi = document.createElement('li')
+                var newP = document.createElement('p')
+                var newA = document.createElement('a')
+                newA.setAttribute('href', item.guid.rendered)
 
-    xhr.withCredentials = true;
-    xhr.onload = function () {
-        var responseText = xhr.responseText;
-        list = JSON.parse(responseText);
-        list.forEach((item, id) => {
-            var newLi = document.createElement('li')
-            var newText = document.createTextNode(`${item.date} ${item.title.rendered}`)
-            newLi.appendChild(newText)
-            document.getElementById('photo-list').appendChild(newLi)
-        })
-    };
-    xhr.onerror = function () {
-        alert('There was an error!');
-    };
-    xhr.send()
-    
-    $('button').click(() => {
-        var xhr1 = createCORSRequest('POST', 'https://nyunews.com/wp-json/wp/v2/posts')
-        xhr1.withCredentials = true;
-        xhr1.send()
+                var newText = document.createTextNode(item.title.rendered)
+                newA.appendChild(newText)
+                newP.appendChild(newA)
+                newLi.appendChild(newP)
+                document.getElementById('photo-list').appendChild(newLi)
+            })
+        },
+        error: (jqXHR, status, error) => {
+            alert(status)
+        }
     })
 
 })
